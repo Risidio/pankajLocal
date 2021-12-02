8abee4f032d2f6a449d9203d9e64ffc6450bbffa
Bara Konateh <bkonateh1@gmail.com>
2021-12-02 16:08:14 +0000

    Features
    Changed the design entirely

--- src/views/marketplace/components/update/ItemFormPart2.vue
+++ src/views/marketplace/components/update/ItemFormPart2.vue
@@ -1,7 +1,22 @@
 <template>
 <div>
+  <div class="mb-3" role="group">
+    <label class="label" for="item-name">Artist / Maker / Creator</label>
+    <b-form-input
+      id="artist-name"
+      v-model="item.artist"
+      :state="itemArtistState"
+      aria-describedby="item-artist-help item-artist-feedback"
+      placeholder="Add a name that will be shown as creator"
+      trim
+      class="inputArea"
+    ></b-form-input>
+    <b-form-invalid-feedback id="item-artist-feedback">
+      Enter the name of the artist
+    </b-form-invalid-feedback>
+  </div>
   <div class="mb-3" role="group text-right w-100">
-    <label for="item-editions">Editions :</label>
+    <label class="label" for="item-editions"> Number of editions :</label>
     <b-form-input
       id="item-editions"
       type="number"
@@ -10,13 +25,14 @@
       aria-describedby="item-editions-help item-editions-feedback"
       placeholder="Enter editions"
       trim
+      class="inputArea max"
     ></b-form-input>
     <b-form-invalid-feedback id="item-editions-feedback">
       How many editions of this NFT do you want to allow? At least 1 - at most 100
     </b-form-invalid-feedback>
   </div>
   <div class="mb-3" role="group text-right w-100">
-    <label for="item-edition-cost">Cost per edition (STX) :</label>
+    <label class="label" for="item-edition-cost">Cost per edition (STX) :</label>
     <b-form-input
       id="item-edition-cost"
       type="number"
@@ -25,6 +41,7 @@
       aria-describedby="item-edition-cost-help item-edition-cost-feedback"
       placeholder="Cost to mint an edition"
       trim
+      class="inputArea max"
     ></b-form-input>
     <b-form-invalid-feedback id="item-editions-feedback">
       How much STX tokens each subsequent edition costs
@@ -51,10 +68,33 @@ export default {
     itemEditionCostState () {
       if (!this.formSubmitted && !this.item.attributes.editionCost) return null
       return (parseInt(this.item.attributes.editionCost) >= 0)
+    },
+    itemNameState () {
+      if (!this.formSubmitted && !this.item.name) return null
+      return (this.item.name && this.item.name.length > 2)
+    },
+    itemArtistState () {
+      if (!this.formSubmitted && !this.item.artist) return null
+      return (this.item.artist && this.item.artist.length > 2)
     }
   }
 }
 </script>
 
 <style scoped>
+.inputArea{
+  width: 100%;
+  border: none;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  font-weight: 200;
+  font-size: 0.8em;
+  padding: 15px;
+}
+.label{
+  font-weight: 500;
+  font-size: 0.8em;
+}
+.max{
+  max-width: 300px;
+}
 </style>
